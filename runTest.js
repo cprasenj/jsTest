0f6cdeb2a0362ba39253638dc4d638497ac7f2ad
Cprasenj <cprasenj@in-cprasenj.local>
2017-12-14 20:04:54 +0530

    fixing compilation errors

--- runTest.js
+++ runTest.js
@@ -1,109 +1,4 @@
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
-82
-83
-84
-85
-86
-87
-88
-89
-90
-91
-92
-93
-94
-95
-96
-97
-98
-99
-100
-101
-102
-103
-	
-//Edited Aug 15,2014
 // Can be used to run all/selected tests.
 // Also can be stopped on first failure
 // Use -list option to list tests
@@ -112,7 +7,7 @@
 // 'node runTest.js exampleTest.js -stop ==> stops on first failure',
 // 'node runTest.js exampleTest.js -only namePart ==> runs all tests that match the namePart'
 // Below are contents of exampleTest.js, a sample test file
-//  
+//
 //-----------exampleTest.js----------------
 //  var assert = require('assert');
 //  var test = {};
@@ -124,7 +19,7 @@
 //  };
 //  exports.test = test;
 //-----------------------------------------
- 
+
 var fs = require('fs');
 var usages = ['node runTest.js exampleTest.js ==> runs all tests',
     'node runTest.js exampleTest.js -list ==> lists all tests',
